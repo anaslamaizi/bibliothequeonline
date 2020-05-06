2c24a0da0bacd4c061eb06f5a9b72cc41ab9b1b9
A681299 <A681299@192.168.1.4>
2020-05-06 02:50:18 +0000

    Initial commit:

---
+++ src/main/java/bebliotheque/online/BebliothequeOnlineApplication.java
@@ -0,0 +1,13 @@
+package bebliotheque.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebliothequeOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BebliothequeOnlineApplication.class, args);
+	}
+
+}
